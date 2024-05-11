d05f7e0f21946e251aee3d2dc1523a2b9a4a54a8
Arthur Ozga <aozgaa@gmail.com>
2024-05-11 17:06:44 -0400

    1.4 textures ref

--- src/1.4.textures_2_texture_units.cpp
+++ src/1.4.textures_2_texture_units.cpp
@@ -2,177 +2,219 @@
 
 #include <GLFW/glfw3.h>
 
-#include "image.h"
-#include "shader_program.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "learnopengl/filesystem.h"
+#include "learnopengl/shader_s.h"
 
 #include <iostream>
-#include <string>
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
+// settings
 const unsigned int SCR_WIDTH  = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderPath   = "src/1.4.textures_2_texture_units.vert";
-const char *fragmentShaderPath = "src/1.4.textures_2_texture_units.frag";
-
-float vertices[] = {
-  // pos              color             texture coords
-  -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // lower left
-  0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // lower right
-  0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // upper right
-  -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, // upper left
-};
-
-unsigned int indices[] = {
-  0, 1, 2, // triangle0
-  0, 2, 3  // triangle1
-};
-
-float borderColor[] = { 1.0, 1.0, 1.0, 1.0 };
-
-int shaderProgram = 0;
-
 int main() {
+  // glfw: initialize and configure
+  // ------------------------------
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 #ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
+  // glfw window creation
+  // --------------------
   GLFWwindow *window =
-      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", nullptr, nullptr);
-  if (window == nullptr) {
-    std::cerr << "failed to create GLFW window" << std::endl;
+      glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
-    exit(1);
+    return -1;
   }
   glfwMakeContextCurrent(window);
-  glfwSetFramebufferSizeCallback(window,
-                                 [](GLFWwindow *window, int width, int height) {
-                                   glViewport(0, 0, width, height);
-                                 });
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  // glad: load all OpenGL function pointers
+  // ---------------------------------------
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cerr << "failed to initialize GLAD" << std::endl;
-    exit(1);
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
   }
 
-  reloadShaders(shaderProgram, vertexShaderPath, fragmentShaderPath);
+  // build and compile our shader zprogram
+  // ------------------------------------
+  Shader ourShader("src/1.4.textures_2_texture_units.vert",
+                   "src/1.4.textures_2_texture_units.frag");
 
-  unsigned int VBO = 0;
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+  float vertices[] = {
+    // positions          // colors           // texture coords
+    0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top right
+    0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom left
+    -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  // top left
+  };
+  unsigned int indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+  };
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  unsigned int VAO = 0;
-  glGenVertexArrays(1, &VAO);
-  glBindVertexArray(VAO);
-
-  unsigned int EBO = 0;
-  glGenBuffers(1, &EBO);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void *)0); // position
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
+  // color attribute
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void *)(3 * sizeof(float))); // colors
+                        (void *)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
+  // texture coord attribute
   glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void *)(6 * sizeof(float))); // texture coords
+                        (void *)(6 * sizeof(float)));
   glEnableVertexAttribArray(2);
 
-  glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind
-  glBindVertexArray(0);             // unbind
+  // load and create a texture
+  // -------------------------
+  unsigned int texture1, texture2;
+  // texture 1
+  // ---------
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+  // set the texture wrapping parameters
+  glTexParameteri(
+      GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+      GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load image, create texture and generate mipmaps
+  int width, height, nrChannels;
+  stbi_set_flip_vertically_on_load(
+      true); // tell stb_image.h to flip loaded texture's on the y-axis.
+  // The FileSystem::getPath(...) is part of the GitHub repository so we can
+  // find files on any IDE/platform; replace it with your own image path.
+  unsigned char *data =
+      stbi_load(FileSystem::getPath("assets/wall.jpg").c_str(), &width, &height,
+                &nrChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+  // texture 2
+  // ---------
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+  // set the texture wrapping parameters
+  glTexParameteri(
+      GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+      GL_REPEAT); // set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load image, create texture and generate mipmaps
+  data = stbi_load(FileSystem::getPath("assets/awesomeface.png").c_str(),
+                   &width, &height, &nrChannels, 0);
+  if (data) {
+    // note that the awesomeface.png has transparency and thus an alpha channel,
+    // so make sure to tell OpenGL the data type is of GL_RGBA
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
 
-  unsigned int wallTexture = 0;
-  glGenTextures(1, &wallTexture);
-  glBindTexture(GL_TEXTURE_2D, wallTexture);
-  auto wallImage = stb::Image("assets/wall.jpg");
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, wallImage.width, wallImage.height, 0,
-               GL_RGB, GL_UNSIGNED_BYTE, wallImage.data);
-  glGenerateMipmap(GL_TEXTURE_2D);
-  auto wallLoc = glGetUniformLocation(shaderProgram, "wallSampler");
+  // tell opengl for each sampler to which texture unit it belongs to (only has
+  // to be done once)
+  // -------------------------------------------------------------------------------------------
+  ourShader.use(); // don't forget to activate/use the shader before setting
+                   // uniforms!
+  // either set it manually like so:
+  glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+  // or set it via the texture class
+  ourShader.setInt("texture2", 1);
 
-  unsigned int smileyTexture = 0;
-  glGenTextures(1, &smileyTexture);
-  glBindTexture(GL_TEXTURE_2D, smileyTexture);
-  auto smileyImage = stb::Image("assets/awesomeface.png");
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, smileyImage.width, smileyImage.height,
-               0, GL_RGBA, GL_UNSIGNED_BYTE, smileyImage.data);
-  glGenerateMipmap(GL_TEXTURE_2D);
-  auto smileyLoc = glGetUniformLocation(shaderProgram, "smileySampler");
-
-  glUseProgram(shaderProgram);
-  glUniform1i(wallLoc, 3);
-  glUniform1i(smileyLoc, 5);
-
-  glBindTexture(GL_TEXTURE_2D, 0); // unbind
-
-  size_t iter = 0;
+  // render loop
+  // -----------
   while (!glfwWindowShouldClose(window)) {
-    iter++;
-    if (fileChanged(vertexShaderPath) || fileChanged(fragmentShaderPath)) {
-      reloadShaders(shaderProgram, vertexShaderPath, fragmentShaderPath);
-    }
-
+    // input
+    // -----
     processInput(window);
 
+    // render
+    // ------
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // set the default texture unit -- should not matter
-    // verifies that smiley+wall are initialized correctly
+    // bind textures on corresponding texture units
     glActiveTexture(GL_TEXTURE0);
-    unsigned int tx0_handle = 0;
-    switch ((iter / 64) % 3) {
-    case 0:
-      tx0_handle = 0;
-      break;
-    case 1:
-      tx0_handle = wallTexture;
-      break;
-    case 2:
-      tx0_handle = smileyTexture;
-      break;
-    }
-    glBindTexture(GL_TEXTURE_2D, tx0_handle);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
 
-    // set the texture units bound to samplers -- does not work!
-    glActiveTexture(GL_TEXTURE0 + 3);
-    glBindTexture(GL_TEXTURE_2D, wallTexture);
-    glActiveTexture(GL_TEXTURE0 + 5);
-    glBindTexture(GL_TEXTURE_2D, smileyTexture);
-
-    glUseProgram(shaderProgram);
+    // render container
+    ourShader.use();
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+    // etc.)
+    // -------------------------------------------------------------------------------
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  glDeleteTextures(1, &smileyTexture);
-  glDeleteTextures(1, &wallTexture);
-  glDeleteBuffers(1, &EBO);
+  // optional: de-allocate all resources once they've outlived their purpose:
+  // ------------------------------------------------------------------------
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
-  glDeleteProgram(shaderProgram);
+  glDeleteBuffers(1, &EBO);
 
+  // glfw: terminate, clearing all previously allocated GLFW resources.
+  // ------------------------------------------------------------------
   glfwTerminate();
   return 0;
 }
 
+// process all input: query GLFW whether relevant keys are pressed/released this
+// frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow *window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
     glfwSetWindowShouldClose(window, true);
-  }
-  if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
-    reloadShaders(shaderProgram, vertexShaderPath, fragmentShaderPath);
-  }
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback
+// function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that width
+  // and height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
 }
--- src/1.4.textures_2_texture_units.frag
+++ src/1.4.textures_2_texture_units.frag
@@ -5,11 +5,11 @@ in vec3 ourColor;
 in vec2 TexCoord;
 
 // texture samplers
-uniform sampler2D wallSampler;
-uniform sampler2D smileySampler;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main() {
-    vec4 c1 = vec4(0.0, 1.0, 0.0, 1.0) * texture(wallSampler, TexCoord);
-    vec4 c2 = vec4(1.0, 0.0, 0.0, 1.0) * (1.0 - texture(smileySampler, TexCoord));
+    vec4 c1 = vec4(0.0, 1.0, 0.0, 1.0) * texture(texture1, TexCoord);
+    vec4 c2 = vec4(1.0, 0.0, 0.0, 1.0) * (1.0 - texture(texture2, TexCoord));
     FragColor = TexCoord.x > 0.5 ? c1 : c2;
 }
